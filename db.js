07e92cb7d911b26cf28aa5d9459de82c22925280
Arslan <muhammadarslan0111@gmail.com>
2022-12-30 15:43:33 +0500

    MONGODB ONLINE setted

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv/config');
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true"
-
-//"mongodb+srv://arslantiwana:Tiwana0111@cluster0.dlzbood.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://cafemanagementsystem:cafemanagementsystem@cluster0.kdjsbvj.mongodb.net/?retryWrites=true&w=majority"
+//mongodb://localhost:27017/?readPreference=primary&directConnection=true
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
